d65b264e1d9d60a77914cc4779b81f624495573d
Yukiwi <117077762+Temiyoko@users.noreply.github.com>
2023-05-10 14:09:26 +0200

    Nommage fichiers

---
+++ FormCode.vb
@@ -0,0 +1,3 @@
+﻿Public Class FormCode
+
+End Class
--- formMenu.Designer.vb
+++ formMenu.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class formMenu
+Partial Class FormMenu
     Inherits System.Windows.Forms.Form
 
     'Form remplace la méthode Dispose pour nettoyer la liste des composants.
