08f7c1ddc1cf6d882e4f4ae0e3f3b805c61b2aa3
Yukiwi <117077762+Temiyoko@users.noreply.github.com>
2023-05-10 14:52:27 +0200

    Modification bouton quitter

--- formMenu.vb
+++ formMenu.vb
@@ -1,6 +1,6 @@
 ﻿Public Class FormMenu
     Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
         ' 276 est le résultat de 4 (Oui/Non) + 16 (Critical) + 256 (2e bouton par défaut)
-        If MsgBox("Voulez-vous vraiment quitter ?", 276, "Quitter") = vbYes Then Me.Close()
+        If MsgBox("Voulez-vous vraiment quitter ?", 276, "Attention") = vbYes Then Me.Close()
     End Sub
 End Class
