b901107032032a272f7772948821f9e27520387c
Yukiwi <117077762+Temiyoko@users.noreply.github.com>
2023-05-10 14:46:21 +0200

    Code bouton quitter

--- FormMenu.vb
+++ FormMenu.vb
@@ -1,3 +1,6 @@
 ﻿Public Class FormMenu
-
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        ' 276 est le résultat de 4 (Oui/Non) + 16 (Critical) + 256 (2e bouton par défaut)
+        If MsgBox("Voulez-vous vraiment quitter ?", 276) = vbYes Then Me.Close()
+    End Sub
 End Class
