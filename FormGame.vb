0b94e30ccc3dfdd3481ebfa5475a900bff8b8ec1
Yukiwi <117077762+Temiyoko@users.noreply.github.com>
2023-05-19 14:54:00 +0200

    Set up code guessing

--- FormGame.vb
+++ FormGame.vb
@@ -15,4 +15,22 @@ Public Class FormGame
         Me.Close()
         FormMenu.Show()
     End Sub
+
+    Private Sub TbCode_TextChanged(sender As Object, e As EventArgs) Handles tbCode1.TextChanged, tbCode2.TextChanged, tbCode3.TextChanged, tbCode4.TextChanged, tbCode5.TextChanged
+        Dim cpt As Integer = 0
+
+        For Each tb As TextBox In pnlTextBox.Controls
+            If Not tb.Text = "" Then
+                Dim nextTb As Integer = (pnlTextBox.Controls.IndexOf(tb) + 1) Mod pnlTextBox.Controls.Count
+                DirectCast(pnlTextBox.Controls(nextTb), TextBox).Focus()
+                cpt += 1
+            End If
+        Next
+
+        If cpt = pnlTextBox.Controls.Count Then
+            btnGuess.Enabled = True
+        Else
+            btnGuess.Enabled = False
+        End If
+    End Sub
 End Class
